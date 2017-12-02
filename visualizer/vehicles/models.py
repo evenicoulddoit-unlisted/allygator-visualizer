47a4462ba1eaf2408f6d4a9750813c390642e709
Ian Clark <ian.clark.001@gmail.com>
2017-12-02 20:34:59 +0000

    Add: First pass at modelling; add vehicles to INSTALLED_APPS

--- visualizer/vehicles/models.py
+++ visualizer/vehicles/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    """
+    Model to store vehicle information and location details.
+    """
+    id = models.CharField(primary_key=True, max_length=36)
+    active = models.BooleanField(
+        default=True, db_index=True,
+        help_text='Whether the vehicle in active service'
+    )
+    current_location = models.PointField(
+        null=True, blank=True,
+        help_text='The coordinates representing the vehicles current location'
+    )
+    current_bearing = models.IntegerField(
+        null=True, blank=True,
+        help_text=(
+            'The current bearing (direction of travel, in degrees) of the '
+            'vehicle'
+        )
+    )
+    last_update_at = models.DateTimeField(
+        null=True, blank=True,
+        help_text='When the vehicle last reported its location'
+    )
+
+
+class VehicleLocation(models.Model):
+    """
+    Model to store each report of a Vehicle's location.
+    """
+    vehicle = models.ForeignKey(
+        'vehicles.Vehicle', on_delete=models.PROTECT,
+        related_name='location_updates',
+        db_index=False,
+    )
+    location = models.PointField()
+    at = models.DateTimeField()
