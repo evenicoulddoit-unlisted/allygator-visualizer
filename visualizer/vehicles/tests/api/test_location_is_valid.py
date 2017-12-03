2c9ba0c02107809a536c417910743c0c9a305417
Ian Clark <ianclark001+github-unlisted@gmail.com>
2017-12-03 01:14:42 +0000

    Fix: Ensure that tests are applying the expected SRID

--- visualizer/vehicles/tests/api/test_location_is_valid.py
+++ visualizer/vehicles/tests/api/test_location_is_valid.py
@@ -4,12 +4,14 @@ from django.test import TestCase
 from vehicles import api as vehicles_api, conf as vehicles_conf
 
 
-# Named locations
-LOC_VALID_VABALI_SPA = Point(13.362, 52.529)
-LOC_INVALID_WICLEF63_CASIO = Point(13.341, 52.531)
-LOC_INVALID_FENNPFUFL_PARK = Point(13.462, 52.531)
-LOC_INVALID_BURGERPARK_PANKOW = Point(13.396, 52.568)
-LOC_INVALID_TEMPELHOFER = Point(13.396, 52.485)
+# Valid location
+LOC_VABALI_SPA = Point(13.362, 52.529, srid=vehicles_conf.SRID_DEFAULT)
+
+# Invalid locations
+LOC_WICLEF63_CASIO = Point(13.341, 52.531, srid=vehicles_conf.SRID_DEFAULT)
+LOC_FENNPFUFL_PARK = Point(13.462, 52.531, srid=vehicles_conf.SRID_DEFAULT)
+LOC_BURGERPARK_PANKOW = Point(13.396, 52.568, srid=vehicles_conf.SRID_DEFAULT)
+LOC_TEMPELHOFER = Point(13.396, 52.485, srid=vehicles_conf.SRID_DEFAULT)
 
 
 class LocationIsValidTests(TestCase):
@@ -26,24 +28,16 @@ class LocationIsValidTests(TestCase):
         self.assertFalse(vehicles_api.location_is_valid(Point(0, 0)))
 
     def test_3km_away_valid(self):
-        self.assertTrue(vehicles_api.location_is_valid(LOC_VALID_VABALI_SPA))
+        self.assertTrue(vehicles_api.location_is_valid(LOC_VABALI_SPA))
 
     def test_over_3km_away_west_invalid(self):
-        self.assertFalse(
-            vehicles_api.location_is_valid(LOC_INVALID_WICLEF63_CASIO)
-        )
+        self.assertFalse(vehicles_api.location_is_valid(LOC_WICLEF63_CASIO))
 
     def test_over_3km_away_east_invalid(self):
-        self.assertFalse(
-            vehicles_api.location_is_valid(LOC_INVALID_FENNPFUFL_PARK)
-        )
+        self.assertFalse(vehicles_api.location_is_valid(LOC_FENNPFUFL_PARK))
 
     def test_over_3km_away_north_invalid(self):
-        self.assertFalse(
-            vehicles_api.location_is_valid(LOC_INVALID_BURGERPARK_PANKOW)
-        )
+        self.assertFalse(vehicles_api.location_is_valid(LOC_BURGERPARK_PANKOW))
 
     def test_over_3km_away_south_invalid(self):
-        self.assertFalse(
-            vehicles_api.location_is_valid(LOC_INVALID_TEMPELHOFER)
-        )
+        self.assertFalse(vehicles_api.location_is_valid(LOC_TEMPELHOFER))
