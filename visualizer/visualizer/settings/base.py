e2cd80e159c272e52e4ac82a58ce7f8821c739cd
Ian Clark <ianclark001+github-unlisted@gmail.com>
2017-12-03 00:57:58 +0000

    Add: Django extensions and iPython to ease debugging

--- visualizer/visualizer/settings/base.py
+++ visualizer/visualizer/settings/base.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Builtins
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
+    'django_extensions',
+
+    # Locals
     'vehicles',
 ]
 
