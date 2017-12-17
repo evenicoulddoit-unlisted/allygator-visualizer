067426aa4f35fa806ad493c0dfc98d6ad12fe554
Ian Clark <ianclark001+github-unlisted@gmail.com>
2017-12-17 23:22:03 +0000

    Add: Allow bootstrapped tests to run

--- ng/src/app/app.component.spec.ts
+++ ng/src/app/app.component.spec.ts
@@ -1,11 +1,28 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, async } from '@angular/core/testing';
+
+import { AgmCoreModule } from '@agm/core';
+
 import { AppComponent } from './app.component';
+import { VehiclesService } from './vehicles.service';
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        AgmCoreModule.forRoot({
+          apiKey: 'AIzaSyAzxNOt5wJZKNcWG9OgdnyueIDuPOFLKsw'
+        }),
+        HttpClientModule,
+        HttpClientTestingModule,
+      ],
+      providers: [VehiclesService],
+
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -17,11 +34,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
